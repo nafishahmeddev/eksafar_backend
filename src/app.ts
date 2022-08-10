db12a34304fc5269a89b6ee891ed0196c64a1c92
nafishahmeddev <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>
2022-08-10 23:56:31 +0530

    fix

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import Models from './models';
 import InquaryFormRoute from './routes/inquiry';
 const app = express();
-const port = 3000;
+const port =  process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
