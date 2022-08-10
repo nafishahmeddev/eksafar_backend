bba25404131a2f72109071af2a50d8d6802ee9d0
nafishahmeddev <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>
2022-08-11 00:11:27 +0530

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from "cors";
 import Models from './models';
 import InquaryFormRoute from './routes/inquiry';
 const app = express();
@@ -8,6 +9,7 @@ const port =  process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 //add routes
 app.use(InquaryFormRoute);
 async function main(){
