1c3195a3deed006d9614750e9ad06f6efcc90355
nafishahmeddev <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>
2022-08-10 23:24:28 +0530

    intial commit

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config'
+import express from 'express';
+import bodyParser from 'body-parser';
+import Models from './models';
+import InquaryFormRoute from './routes/inquiry';
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+//add routes
+app.use(InquaryFormRoute);
+async function main(){
+
+  await Models();
+  app.listen(port, () => {
+    return console.log(`Express is listening at http://localhost:${port}`);
+  });
+}
+
+main();
